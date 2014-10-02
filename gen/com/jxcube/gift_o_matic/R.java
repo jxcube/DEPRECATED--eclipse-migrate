1abe14d23c5906e8f3db8c53b0ca64c8ef676025
Muhammad Zulfikar <izuleonel@gmail.com>
2014-10-02 12:11:49 +0700

    Adding volley jar to library

--- gen/com/jxcube/gift_o_matic/R.java
+++ gen/com/jxcube/gift_o_matic/R.java
@@ -12,37 +12,56 @@ public final class R {
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
-
-         Example customization of dimensions originally defined in res/values/dimens.xml
+ Example customization of dimensions originally defined in res/values/dimens.xml
          (such as screen margins) for screens with more than 820dp of available width. This
-         would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
-    
+         would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively). 
          */
         public static final int activity_horizontal_margin=0x7f040000;
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
         public static final int email_button=0x7f020000;
-        public static final int email_button_2=0x7f020001;
-        public static final int fb_button=0x7f020002;
+        public static final int fb_button=0x7f020001;
+        public static final int google_button=0x7f020002;
         public static final int ic_launcher=0x7f020003;
         public static final int logo=0x7f020004;
     }
     public static final class id {
-        public static final int action_settings=0x7f080009;
-        public static final int email_field=0x7f080005;
-        public static final int imageButton=0x7f080001;
-        public static final int imageButton11=0x7f080000;
-        public static final int imageView=0x7f080002;
-        public static final int password_field=0x7f080007;
-        public static final int password_repeat_field=0x7f080008;
-        public static final int textView=0x7f080003;
-        public static final int textView2=0x7f080004;
-        public static final int username_field=0x7f080006;
+        public static final int SignInLayout=0x7f080006;
+        public static final int Signintext=0x7f080007;
+        public static final int action_settings=0x7f08001b;
+        public static final int dobField=0x7f080018;
+        public static final int emailField=0x7f080003;
+        public static final int emailText=0x7f080002;
+        public static final int email_btn=0x7f08000a;
+        public static final int f_radioBtn=0x7f080016;
+        public static final int fb_btn=0x7f080008;
+        public static final int gplus_btn=0x7f080009;
+        public static final int imageView=0x7f08001a;
+        public static final int imageView2=0x7f080000;
+        public static final int linearLayout4=0x7f08000f;
+        public static final int loginText=0x7f080001;
+        public static final int login_btn=0x7f08000c;
+        public static final int logoImg=0x7f08000e;
+        public static final int logoLayout=0x7f08000d;
+        public static final int m_radioBtn=0x7f080015;
+        public static final int orText=0x7f08000b;
+        public static final int passwordField=0x7f080005;
+        public static final int passwordText=0x7f080004;
+        public static final int textView=0x7f080011;
+        public static final int textView2=0x7f080013;
+        public static final int textView3=0x7f080014;
+        public static final int textView4=0x7f080017;
+        public static final int textView5=0x7f080019;
+        public static final int textView7=0x7f080010;
+        public static final int usernameField=0x7f080012;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
-        public static final int activity_register_email=0x7f030001;
+        public static final int activity_login=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int activity_signin=0x7f030002;
+        public static final int activity_signup=0x7f030003;
+        public static final int splash=0x7f030004;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -50,29 +69,22 @@ public final class R {
     }
     public static final class string {
         public static final int action_settings=0x7f050002;
+        public static final int action_sign_in=0x7f050006;
+        public static final int action_sign_in_short=0x7f050007;
         public static final int app_name=0x7f050000;
-        public static final int email=0x7f050007;
-        public static final int facebook_btn=0x7f050005;
+        public static final int error_field_required=0x7f05000b;
+        public static final int error_incorrect_password=0x7f05000a;
+        public static final int error_invalid_email=0x7f050008;
+        public static final int error_invalid_password=0x7f050009;
         public static final int hello_world=0x7f050001;
-        public static final int or=0x7f050004;
-        public static final int password_hint=0x7f050008;
-        public static final int password_repeat_hint=0x7f05000a;
-        public static final int signup_text=0x7f050003;
-        public static final int submit=0x7f05000b;
-        public static final int title_activity_register_email=0x7f050006;
-        public static final int username_hint=0x7f050009;
+        /**  Strings related to login 
+         */
+        public static final int prompt_email=0x7f050004;
+        public static final int prompt_password=0x7f050005;
+        public static final int title_activity_login=0x7f050003;
     }
     public static final class style {
         /** 
-        Base application theme, dependent on API level. This theme is replaced
-        by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
-    
-
-            Theme customizations available in newer API levels can go in
-            res/values-vXX/styles.xml, while customizations related to
-            backward-compatibility can go here.
-        
-
         Base application theme for API 11+. This theme completely replaces
         AppBaseTheme from res/values/styles.xml on API 11+ devices.
     
@@ -84,10 +96,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
-        /**  Application theme. 
- All customizations that are NOT specific to a particular API-level can go here. 
+        public static final int AppBaseTheme=0x7f060001;
+        /**  Base application theme. 
+ Customize your theme here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f060000;
     }
 }
